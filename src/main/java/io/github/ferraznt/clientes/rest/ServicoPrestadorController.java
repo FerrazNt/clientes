616afcddb65afd863a4f8028b3d0a0b7390a129e
Gregório Ferraz <jgregoriof@minsait.com>
2023-02-23 09:12:11 -0300

    Alterações realizadas até a aula 95: - Criação da API de Serviço Prestado. - Criação do WS de Retorno de Lista de Serviços por Mês e Nome.

--- src/main/java/io/github/ferraznt/clientes/rest/ServicoPrestadorController.java
+++ src/main/java/io/github/ferraznt/clientes/rest/ServicoPrestadorController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -33,7 +34,7 @@ public class ServicoPrestadorController {
 
     @PostMapping("/novo")
     @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto){
+    public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO dto){
         LocalDate data = LocalDate.parse(dto.getDataServico(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         Integer idCliente = dto.getIdCliente();
 
--- src/main/java/io/github/ferraznt/clientes/rest/dto/ServicoPrestadoDTO.java
+++ src/main/java/io/github/ferraznt/clientes/rest/dto/ServicoPrestadoDTO.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServicoPrestadoDTO {
+
+    @NotNull(message = "{not_null.campo.descricao}")
     private String descricao;
+
+    @NotNull(message = "{not_null.campo.valor}")
     private String valor;
+
+    @NotNull(message = "{not_null.campo.data-servico}")
     private String dataServico;
+
+    @NotNull(message = "{not_null.campo.id-cliente}")
     private Integer idCliente;
 }
